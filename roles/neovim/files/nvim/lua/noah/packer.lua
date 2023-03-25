086974b058690ae4d77d156cc12f4420bbe06dd4
root <root@Noahs-Pc.localdomain>
2023-03-25 13:27:01 +0100

    Feat: Start own neovim config

---
+++ roles/neovim/files/nvim/lua/noah/packer.lua
@@ -0,0 +1,21 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use {
+	'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use ({ 
+	  "catppuccin/nvim",
+	  as = "catppuccin",
+	  config = function()
+		  vim.cmd("colorscheme catppuccin-macchiato")
+	  end
+  })
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+end)
