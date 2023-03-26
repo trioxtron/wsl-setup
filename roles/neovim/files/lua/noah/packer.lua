6a1adfabc883e6ccbc381691b492a37ce5e6f4bc
trioxtron <trioxgames@gmail.com>
2023-03-26 15:11:39 +0200

    Feat: Add plugin 'nvim-tree'

--- roles/neovim/files/lua/noah/packer.lua
+++ roles/neovim/files/lua/noah/packer.lua
@@ -21,6 +21,8 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use('nvim-tree/nvim-tree.lua')
+  use('nvim-tree/nvim-web-devicons')
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
