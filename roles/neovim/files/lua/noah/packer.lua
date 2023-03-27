2bc6c0878e8cce7a09d6909ceca80f4989c0b741
trioxtron <trioxgames@gmail.com>
2023-03-27 23:24:21 +0200

    Feat: add trouble plugin

--- roles/neovim/files/lua/noah/packer.lua
+++ roles/neovim/files/lua/noah/packer.lua
@@ -1,44 +1,51 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use {
-	'nvim-telescope/telescope.nvim', tag = '0.1.0',
-  	requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use ("catppuccin/nvim")
+    use("catppuccin/nvim")
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use('nvim-tree/nvim-tree.lua')
-  use('nvim-tree/nvim-web-devicons')
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use('nvim-tree/nvim-tree.lua')
+    use('nvim-tree/nvim-web-devicons')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-	    -- LSP Support
-	    {'neovim/nvim-lspconfig'},             -- Required
-	    {'williamboman/mason.nvim'},           -- Optional
-	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },    -- Required
+            { 'williamboman/mason.nvim' },  -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-	    -- Autocompletion
-	    {'hrsh7th/nvim-cmp'},         -- Required
-	    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-	    {'hrsh7th/cmp-buffer'},       -- Optional
-	    {'hrsh7th/cmp-path'},         -- Optional
-	    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-	    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-buffer' }, -- Optional
+            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-	    -- Snippets
-	    {'L3MON4D3/LuaSnip'},             -- Required
-	    {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
-  }
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },    -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    }
 
+    use {
+        'folke/trouble.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require 'trouble'.setup {}
+        end
+    }
 end)
