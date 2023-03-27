ec840bd895ff295f72a4ae4307eef15e687063a4
trioxtron <trioxgames@gmail.com>
2023-03-27 22:50:48 +0200

    Feat: adjust lsp config

--- roles/neovim/files/after/plugin/lsp.lua
+++ roles/neovim/files/after/plugin/lsp.lua
@@ -3,8 +3,9 @@ local lsp = require('lsp-zero').preset({
   set_lsp_keymaps = true,
   manage_nvim_cmp = true,
   suggest_lsp_servers = false,
-
 })
+local opts = { noremap = true, silent = true}
+
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -15,11 +16,11 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 })
 
 lsp.on_attach(function (client, bufnr)
-    local opts = {buffer = bufnr, remap = false} 
-    vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function () vim.lsp.buf.hover() end, opts)
-
-
+    local bufopts = {buffer = bufnr, remap = false} 
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format() end, bufopts)
 end)
 
 lsp.setup()
