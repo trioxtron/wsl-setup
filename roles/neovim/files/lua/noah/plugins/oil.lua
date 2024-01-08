0d882f9e1aa8a3f5a06b027326826ca3a2a7d933
trioxtron <trioxgames@gmail.com>
2024-01-08 17:14:25 +0100

    feat(nvim): toggle oil shortcut

--- roles/neovim/files/lua/noah/plugins/oil.lua
+++ roles/neovim/files/lua/noah/plugins/oil.lua
@@ -2,6 +2,11 @@ return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function ()
-        require("oil").setup()
+        local oil = require("oil")
+        oil.setup()
+
+        vim.keymap.set("n", "<leader>tt", function ()
+            oil.toggle_float()
+        end)
     end
 }
