9efcedc65ac27949f426871e74086235527c3fa4
trioxtron <trioxgames@gmail.com>
2024-01-09 16:42:39 +0100

    feat(nvim): update harpoon to harpoon 2

--- roles/neovim/files/lua/noah/plugins/harpoon.lua
+++ roles/neovim/files/lua/noah/plugins/harpoon.lua
@@ -1,17 +1,24 @@
 return {
     "theprimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
 
     config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+        local harpoon = require("harpoon")
 
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        vim.keymap.set("n", "<C-i>", ui.toggle_quick_menu)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<C-i>", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+        -- Previous and next buffer
+        vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
     end
 }
 
