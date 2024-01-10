0a7e022b676966fb11bee51b40d4b495ba8c8be6
trioxtron <trioxgames@gmail.com>
2024-01-10 18:53:34 +0100

    feat(nvim): change harpoon ui to telescope

--- roles/neovim/files/lua/noah/plugins/harpoon.lua
+++ roles/neovim/files/lua/noah/plugins/harpoon.lua
@@ -2,14 +2,42 @@ return {
     "theprimeagen/harpoon",
     branch = "harpoon2",
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
     },
 
     config = function()
         local harpoon = require("harpoon")
+        harpoon:setup({
+            settings = {
+                save_on_toggle = true,
+                sync_on_toggle = true,
+            },
+        })
+
+        -- basic telescope configuration
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            }):find()
+        end
+
+        --vim.keymap.set("n", "<C-i>", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<C-i>", function () toggle_telescope(harpoon:list()) end)
+
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<C-i>", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
