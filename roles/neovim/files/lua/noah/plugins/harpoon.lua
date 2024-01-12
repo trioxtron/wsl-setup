37183c5f3a34353dbb1ad9ff7e16d85a7e2aa69f
trioxtron <trioxgames@gmail.com>
2024-01-12 12:57:01 +0100

    undo(nvim): reverted telescope listing of harpoon

--- roles/neovim/files/lua/noah/plugins/harpoon.lua
+++ roles/neovim/files/lua/noah/plugins/harpoon.lua
@@ -3,7 +3,6 @@ return {
     branch = "harpoon2",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim",
     },
 
     config = function()
@@ -15,27 +14,8 @@ return {
             },
         })
 
-        -- basic telescope configuration
-        local conf = require("telescope.config").values
-        local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-                table.insert(file_paths, item.value)
-            end
 
-            require("telescope.pickers").new({}, {
-                prompt_title = "Harpoon",
-                finder = require("telescope.finders").new_table({
-                    results = file_paths,
-                }),
-                previewer = conf.file_previewer({}),
-                sorter = conf.generic_sorter({}),
-            }):find()
-        end
-
-        --vim.keymap.set("n", "<C-i>", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        vim.keymap.set("n", "<C-i>", function () toggle_telescope(harpoon:list()) end)
-
+        vim.keymap.set("n", "<C-i>", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 
